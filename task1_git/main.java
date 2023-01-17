50a691a821864e1046420ca05de1976b6cad5a4f
Jaiprathik Soda <jaiprathik_soda@epam.com>
2023-01-17 22:41:59 +0530

    created main.java file

---
+++ task1_git/main.java
@@ -0,0 +1,12 @@
+public class main
+{
+public static void main(String args[])
+{
+   Student s = new Student();
+   s.setRollno(1);
+   s.setName("Prathik");
+   System.out.println(s.getRollno());
+   System.out.println(s.getName());
+
+}
+}
