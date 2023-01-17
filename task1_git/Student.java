0a6bf99f6ac200b15f33226af59dd0b2f660404e
Jaiprathik Soda <jaiprathik_soda@epam.com>
2023-01-17 22:44:55 +0530

    student.java file is created

---
+++ task1_git/Student.java
@@ -0,0 +1,23 @@
+public class Student
+{
+private int rollno;
+private String name;
+
+public void setRollno(int r)
+{
+  rollno = r;
+}
+public int getRollno()
+{
+   return rollno;
+}
+public void setName(String n)
+{
+  name = n;
+}
+public String getName()
+{
+   return name;
+}
+
+}
